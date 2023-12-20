9261745a079a27c9fbdfd19c0d28a5e0bc05ebeb
Nimish Karn <150058194+Nimish-Codes@users.noreply.github.com>
2023-12-20 21:09:42 +0545

    Update Future_sales_predict.py

--- Future_sales_predict.py
+++ Future_sales_predict.py
@@ -2,11 +2,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
+from datetime import datetime, timedelta
 
 # Generate synthetic sales data
 np.random.seed(40)
@@ -41,12 +40,9 @@ model = LinearRegression()
 # Train the model
 model.fit(X_train, y_train)
 
-# Make predictions on the test set
-y_pred = model.predict(X_test)
-
 # Streamlit App
-st.title('Sales Prediction Model Evaluation')
-st.write('This app evaluates a linear regression model for sales prediction.')
+st.title('Sales Prediction Model and Forecasting')
+st.write('This app evaluates a linear regression model for sales prediction and forecasts future sales.')
 
 # Display sample data
 st.subheader('Sample Data:')
@@ -58,33 +54,32 @@ st.write(sales_data[['Date', 'Year', 'Month', 'Day']].head(7))
 
 # Evaluate the model
 st.subheader('Model Evaluation:')
-mse = mean_squared_error(y_test, y_pred)
+mse = mean_squared_error(y_test, model.predict(X_test))
 st.write(f'Mean Squared Error: {mse}')
 
 # Visualize the results
 st.subheader('Sales Prediction Visualization:')
 fig, ax = plt.subplots(figsize=(10, 6))
 ax.scatter(X_test.index, y_test, color='black', label='Actual Sales')
-ax.plot(X_test.index.values, y_pred, color='blue', linewidth=3, label='Predicted Sales')
+ax.plot(X_test.index.values, model.predict(X_test), color='blue', linewidth=3, label='Predicted Sales')
 ax.set_title('Sales Prediction')
 ax.set_xlabel('Index')
 ax.set_ylabel('Sales')
 ax.legend()
 st.pyplot(fig)
 
-# Visualize the results in 3D
-st.subheader('3D Sales Prediction Visualization:')
-fig_3d = plt.figure(figsize=(12, 8))
-ax_3d = fig_3d.add_subplot(111, projection='3d')
-ax_3d.scatter(X_test['Year'], X_test['Month'], y_test, color='black', label='Actual Sales')
-ax_3d.scatter(X_test['Year'], X_test['Month'], y_pred, color='blue', label='Predicted Sales')
-ax_3d.set_xlabel('Year')
-ax_3d.set_ylabel('Month')
-ax_3d.set_zlabel('Sales')
-ax_3d.set_title('3D Sales Prediction')
-ax_3d.legend()
-st.pyplot(fig_3d)
+# Forecast future sales
+st.subheader('Forecast Future Sales:')
+st.write('Enter a future date to forecast sales:')
+future_date = st.date_input('Select a future date:', min_value=datetime.today() + timedelta(days=1))
 
-# Provide information about using the trained model for forecasting
-st.subheader('Forecasting Future Sales:')
-st.write('Now you can use the trained model to forecast future sales by providing future dates as input features.')
+# Extract features from the future date
+future_year = future_date.year
+future_month = future_date.month
+future_day = future_date.day
+
+# Make predictions for the future date
+future_sales_prediction = model.predict([[future_year, future_month, future_day]])[0]
+
+# Display the forecasted sales
+st.write(f'Forecasted Sales for {future_date}: {future_sales_prediction}')
