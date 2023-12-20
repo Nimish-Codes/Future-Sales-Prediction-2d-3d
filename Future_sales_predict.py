3b27691595f51817ca5da6ff91f114f0947fb7d4
Nimish Karn <150058194+Nimish-Codes@users.noreply.github.com>
2023-12-20 21:33:40 +0545

    Update Future_sales_predict.py

--- Future_sales_predict.py
+++ Future_sales_predict.py
@@ -1,19 +1,15 @@
-# Import necessary libraries
+import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-import streamlit as st
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import numpy as np
-from datetime import datetime, timedelta
+from mpl_toolkits.mplot3d import Axes3D
 
 # Generate synthetic sales data
 np.random.seed(40)
-
-# Generate dates for the last two years
 dates = pd.date_range('2022-01-01', '2023-12-31', freq='D')
-
-# Generate synthetic sales data
 sales_data = pd.DataFrame({
     'Date': dates,
     'Sales': np.random.randint(5, 200, size=len(dates)) + np.sin(np.arange(len(dates))) * 20,
@@ -21,65 +17,60 @@ sales_data = pd.DataFrame({
     'Product': np.random.randint(1, 5, size=len(dates))
 })
 
-# Feature engineering: Extracting useful features
+# Feature engineering: Extract useful features
 sales_data['Year'] = sales_data['Date'].dt.year
 sales_data['Month'] = sales_data['Date'].dt.month
 sales_data['Day'] = sales_data['Date'].dt.day
 
-# Split the data into training and testing sets
+# Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(
-    sales_data[['Year', 'Month', 'Day']],
-    sales_data['Sales'],
-    test_size=0.2,
-    random_state=42
+    sales_data[['Year', 'Month', 'Day']], sales_data['Sales'], test_size=0.2, random_state=42
 )
 
-# Create a linear regression model
+# Create and train a linear regression model
 model = LinearRegression()
-
-# Train the model
 model.fit(X_train, y_train)
 
-# Streamlit App
-st.title('Sales Prediction Model and Forecasting')
-st.write('This app evaluates a linear regression model for sales prediction and forecasts future sales.')
+# Make predictions on test set and evaluate model
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
 
-# Display sample data
-st.subheader('Sample Data:')
-st.write(sales_data.head(7))
+# Display results
+st.title("Sales Prediction and Forecast")
 
-# Display feature engineering results
-st.subheader('Feature Engineering:')
-st.write(sales_data[['Date', 'Year', 'Month', 'Day']].head(7))
+# Show code
+st.code(open('sales_forecast.py').read(), language='python')
 
-# Evaluate the model
-st.subheader('Model Evaluation:')
-mse = mean_squared_error(y_test, model.predict(X_test))
-st.write(f'Mean Squared Error: {mse}')
+# Show head of sales data
+st.header("Sales Data")
+st.dataframe(sales_data.head(7))
 
-# Visualize the results
-st.subheader('Sales Prediction Visualization:')
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.scatter(X_test.index, y_test, color='black', label='Actual Sales')
-ax.plot(X_test.index.values, model.predict(X_test), color='blue', linewidth=3, label='Predicted Sales')
-ax.set_title('Sales Prediction')
-ax.set_xlabel('Index')
-ax.set_ylabel('Sales')
-ax.legend()
-st.pyplot(fig)
+# Show Mean Squared Error
+st.metric(label="Mean Squared Error", value=mse)
 
-# Forecast future sales
-st.subheader('Forecast Future Sales:')
-st.write('Enter a future date to forecast sales:')
-future_date = st.date_input('Select a future date:', min_value=datetime.today() + timedelta(days=1))
+# Show 2D sales prediction plot
+st.plotly_chart(plt.figure(figsize=(10, 6)))
+plt.scatter(X_test.index, y_test, color='black', label='Actual Sales')
+plt.plot(X_test.index, y_pred, color='blue', linewidth=3, label='Predicted Sales')
+plt.title('Sales Prediction')
+plt.xlabel('Index')
+plt.ylabel('Sales')
+plt.legend()
+st.pyplot()
 
-# Extract features from the future date
-future_year = future_date.year
-future_month = future_date.month
-future_day = future_date.day
+# Show 3D sales prediction plot
+st.plotly_chart(fig)
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X_test['Year'], X_test['Month'], y_test, color='black', label='Actual Sales')
+ax.scatter(X_test['Year'], X_test['Month'], y_pred, color='blue', label='Predicted Sales')
+ax.set_xlabel('Year')
+ax.set_ylabel('Month')
+ax.set_zlabel('Sales')
+ax.set_title('3D Sales Prediction')
+plt.legend()
+st.pyplot()
 
-# Make predictions for the future date
-future_sales_prediction = model.predict([[future_year, future_month, future_day]])[0]
+# Forecast message
+st.info("Now you can use this trained model to forecast future sales by providing future dates as input features!")
 
-# Display the forecasted sales
-st.write(f'Forecasted Sales for {future_date}: {future_sales_prediction}')
